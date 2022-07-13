13366c3789795605d4ff0d18cbafad57baa48488
Serhat Can <85739464+serhatc4n@users.noreply.github.com>
2022-07-13 23:52:58 +0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,18 @@ const clock = setInterval(function time()  {
   let hr = dateToday.getHours();
   let min = dateToday.getMinutes();
   let sec = dateToday.getSeconds();
+  
+  if(hr < 10) {
+    hr = '0' + hr
+  }
+  
+  if(min < 10) {
+    min = '0' + min
+  }
+  
+  if(sec < 10) {
+    sec = '0' + sec
+  }
 
   hour.textContent = hr;
   minunte.textContent = min;
